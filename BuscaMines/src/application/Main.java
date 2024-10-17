7ddce4976235e092cf6bf7843bb3b53d48a00a26
User <User@DESKTOP-LKBTRPR>
2024-10-17 18:44:38 +0200

    Creacio de l'estructura de les classes principals, Controlador, Model i vista

--- BuscaMines/src/application/Main.java
+++ BuscaMines/src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
-	
+
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -15,6 +16,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			Controlador c = new Controlador();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
