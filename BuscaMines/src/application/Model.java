1d11ab8c1dc10ce22361f3feb477913755c3d727
User <User@DESKTOP-LKBTRPR>
2024-10-24 07:54:51 +0200

    metodes interficie Model

--- BuscaMines/src/application/Model.java
+++ BuscaMines/src/application/Model.java
@@ -1,5 +1,12 @@
 package application;
 
 public interface Model {
+	
+	void inicialitzaMatvalors(int i, int j); //inicialitza la matriu de valors 
+	boolean isPrimerClic();//mira la matriu de disponibilitat
+	int[][] getValosr(int i, int j);//Dona un array d'arrays amb tres pocision [[valor, posX,posY],...], ja que cuan clici una posciocio buida s'expandeixi els clics. Nomes retornara les pocicions que encara no han estat seleccionades.
+	
+	
+	
 
 }
