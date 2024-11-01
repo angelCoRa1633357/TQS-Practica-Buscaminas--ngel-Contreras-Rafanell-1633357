bb0ca4da8a7322dce811f2c7eff3c6b5c40a644e
User <User@DESKTOP-LKBTRPR>
2024-11-01 11:40:51 +0100

    Creacio interficie, TaulerDisponibilitat i Tauler de valors, i implementacio de la clase Buscamines que implementa la interficie Model.

---
+++ BuscaMines/src/application/BuscaminesModel.java
@@ -0,0 +1,46 @@
+package application;
+
+public class BuscaminesModel implements Model {
+	private int llargada;
+	private int amplitud;
+	private int numMines;
+	private TaulerValors TaulerV;
+	private TaulerDisponibilitat TaulerD; 
+	
+	
+	public BuscaminesModel(int llargada, int amplitud, int numMines, TaulerValors Tv,TaulerDisponibilitat Td ) {
+		
+	}
+	
+	
+	public void inicialitzaMatvalors(int i, int j) {
+		
+	}
+	
+	public boolean isPrimerClic() {
+		return true; //valor temporal
+	}
+	public int[][] getValosr(int i, int j){
+		int v[][] = {{0}};
+		return v; //valor temporal
+	}
+	public boolean isBomba(int i, int j) {
+		return true; //valor temporal
+	}
+	
+	private int getLlargada() {
+		return 0;
+	}
+	
+	private int getAmplada() {
+		return 0;
+	}
+	
+	private int getNumMines() {
+		return 0;
+	}
+	
+	
+	
+
+}
